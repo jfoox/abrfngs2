e76fc195c2f8993a8f1d309bbe4305a1bf856bda
Jonathan Foox <jfoox@amnh.org>
2021-03-31 10:03:10 -0400

    QC and Mapping

--- Rmds/QCandMapping.R
+++ Rmds/QCandMapping.R
@@ -1,6 +1,4 @@
 # Dependencies
-
-```{r, message=F}
 library(ggplot2)
 library(dplyr)
 library(tidyr)
@@ -10,9 +8,7 @@ library(circlize)
 library(ggbeeswarm)
 library(cowplot)
 library(ggpubr)
-```
 
-```{r}
 theme_linedraw2 = theme_linedraw() + theme(strip.background=element_rect(fill="grey80", colour="grey50", size=0.2), strip.text.x=element_text(colour="black"), strip.text.y=element_text(colour="black"))
 theme_jon = theme_linedraw2 + 
   theme(legend.position="bottom", 
@@ -31,68 +27,41 @@ theme_jon = theme_linedraw2 +
 platform.colors = c("BGISEQ500"="#fd8d3c", "HiSeq2500"="#6baed6", "HiSeq4000"="#3182bd", "HiSeqX10"="#08519c", "MGISEQ2000"="#e6550d", "NovaSeq2x150"="#f768a1", "NovaSeq2x250"="#ae017e", "Genapsys"="#f3d04a", "S5"="#67a9cf", "Proton"="#1c9099", "PacBio" = "#66c2a4", "PromethION"="#006d2c", "MinION"="#74c476", "Flongle"="#c7e9c0", "MiSeq"="#08519c", "S5"="#67a9cf", "PGM"="#1fb597")
 bacteria.colors = c("MiSeq"="#4daf4a", "Genapsys"="#ff7f00", "S5"="#377eb8", "PGM"="#984ea3")
 
-
 normalized<-function(y) {
   x<-y[!is.na(y)]
   x<-(x - min(x)) / (max(x) - min(x))
   y[!is.na(y)]<-x
   return(y)
   }
-```
 
-# Mapping Stats from Sentieon
 
-```{r}
+# Mapping Stats
 read_labels = c("pctProperlyPaired"="Uniquely Mapped", "pctDuplicated"="Duplicates", "pctMultimap"="Multi-Mapped", "pctUnmap"="Unmapped")
 readType_labels = c("short"="Short Reads", "long"="Long Reads", "shallow"="Shallow", "exome"="Exome", "bact"="Bacterial")
-
 df_map = read.csv("tables/mapping_stats_all_v2.csv")
 df_map = separate(df_map, col="sample", sep="_", into=c("platform", "lab", "genome", "rep"), remove=F)
 
-# label short, long, and exome reads
+## label short, long, and exome reads
 df_map$readType = ifelse(df_map$platform %in% c("PromethION", "PacBio"), "long", ifelse(df_map$platform %in% c("S5", "Proton"), "exome", ifelse(df_map$platform %in% c("Flongle", "MinION", "Genapsys"), "shallow", "short")))
 
-# get total bases / mean depth
+## get total bases / mean depth
 df_map$totalBases = as.numeric(df_map$readsTotal * df_map$avgLength)
 df_map$meanDepth  = ifelse(df_map$readType == "exome", df_map$totalBases/60470914, df_map$totalBases/3100000000)
 
-# get percentages
+## get percentages
 df_map$pctProperlyPaired = df_map$readsProperlyPaired / df_map$readsTotal * 100
 df_map$pctDuplicated = df_map$readsDuplicated / df_map$readsTotal * 100
 df_map$pctMultimap = df_map$readsMultimap / df_map$readsTotal * 100
 df_map$pctUnmap = df_map$readsUnmap / df_map$readsTotal * 100
 
-# re-order factors
+## re-order factors
 df_map$readType = factor(df_map$readType, levels=c("short", "shallow", "long", "exome"))
 df_map$platform = factor(df_map$platform, levels=c("HiSeq2500", "HiSeq4000", "HiSeqX10", "BGISEQ500", "MGISEQ2000", "Genapsys", "NovaSeq2x150", "NovaSeq2x250", "S5", "Proton", "PacBio", "Flongle", "MinION",  "PromethION"))
 
-# adjust PB for aesthetics so points don't go over lines
+## adjust PB for aesthetics so points don't go over lines
 df_map[df_map$platform=="PacBio", ]$pctProperlyPaired = df_map[df_map$platform=="PacBio", ]$pctProperlyPaired - 0.1
 
-# bacterial
-df_map_bact = read.csv("tables/mapping_stats_bacteria.csv")
-df_map_bact = separate(df_map_bact, col="sample", sep="_", into=c("platform", "lab", "genome", "rep"), remove=F)
-# label paired and single end
-df_map_bact$readType = ifelse(df_map_bact$platform == "MiSeq", "paired", "single")
-df_map_bact$readType2 = "bact"
-# get total bases / mean depth
-df_map_bact$totalBases = as.numeric(df_map_bact$readsTotal) * as.numeric(df_map_bact$avgLength)
-df_map_bact$meanDepth  = ifelse(df_map_bact$genome == "Eco", df_map_bact$totalBases/4641652, 
-                                ifelse(df_map_bact$genome == "Ste", df_map_bact$totalBases/2499279, 
-                                       ifelse(df_map_bact$genome == "Pflu", df_map_bact$totalBases/6511547, df_map_bact$totalBases/39879740)))
-
-# get percentages
-df_map_bact$pctProperlyPaired = df_map_bact$readsProperlyPaired / df_map_bact$readsTotal * 100
-df_map_bact$pctDuplicated = df_map_bact$readsDuplicated / df_map_bact$readsTotal * 100
-df_map_bact$pctMultimap = df_map_bact$readsMultimap / df_map_bact$readsTotal * 100
-df_map_bact$pctUnmap = df_map_bact$readsUnmap / df_map_bact$readsTotal * 100
-# re-order factors
-df_map_bact$platform = factor(df_map_bact$platform, levels=c("MiSeq", "Genapsys", "S5", "PGM"))
-df_map_bact$genome = factor(df_map_bact$genome, levels=c("Eco", "Ste", "Pflu", "Pool"))
-df_map_bact$readType = factor(df_map_bact$readType, levels=c("paired", "single"))
-```
-
-```{r, fig.width=12, fig.height=3}
+## Plot depth of coverage
 gg_bwameth_depth_1 = ggplot(subset(df_map, readType=="short"), aes(x=platform, y=meanDepth)) +
   geom_bar(stat="identity", aes(fill=platform, color=sample), size=0.1, show.legend = F, position = "dodge") +
   theme_jon + theme(axis.text.x = element_text(size=10), axis.title.y = element_text(size=12)) +
@@ -130,17 +99,15 @@ gg_bwameth_depth_5 = ggplot(subset(df_map_bact, readType=="single" | readType=="
   scale_y_log10() +
   xlab("") + ylab("") 
 gg_bwameth_depth = plot_grid(gg_bwameth_depth_1, gg_bwameth_depth_2, gg_bwameth_depth_3, gg_bwameth_depth_4, gg_bwameth_depth_5, nrow=1, align = 'h', rel_widths=c(4, 1.5, 1.5, 2.5, 4))
-gg_bwameth_depth
-```
-```{r, fig.width=10, fig.height=8}
+
+## Export
 pdf(file="figures/Fig1b_Depth.pdf", width=12, height=3)
 gg_bwameth_depth
 dev.off()
-```
 
 
-```{r, fig.width=12, fig.height=3}
-gg_bwameth_map = ggplot(subset(df_map, pctProperlyPaired > 92), aes(x=platform, y=pctProperlyPaired)) +
+## Plot mapping stats
+gg_bwameth_map = ggplot(df_map, aes(x=platform, y=pctProperlyPaired)) +
   geom_boxplot(aes(fill=platform, color=platform), alpha=0.5, show.legend=F) +
   geom_beeswarm(aes(fill=platform, shape=genome), alpha=0.5, show.legend=F, size=3) +
   geom_vline(xintercept = c(8.5, 10.5), linetype="dotted") +
@@ -152,7 +119,7 @@ gg_bwameth_map = ggplot(subset(df_map, pctProperlyPaired > 92), aes(x=platform, 
   xlab("") + ylab("% Read Pairs") +
   ggtitle("Properly Mapped")
 
-gg_bwameth_multi = ggplot(subset(df_map, pctMultimap < 7), aes(x=platform, y=pctMultimap)) +
+gg_bwameth_multi = ggplot(df_map, aes(x=platform, y=pctMultimap)) +
   geom_boxplot(aes(color=platform, fill=platform), alpha=0.5, show.legend=F) +
   geom_beeswarm(aes(fill=platform, shape=genome), alpha=0.5, show.legend=F, size=3) +
   geom_vline(xintercept = c(8.5, 10.5), linetype="dotted") +
@@ -164,7 +131,7 @@ gg_bwameth_multi = ggplot(subset(df_map, pctMultimap < 7), aes(x=platform, y=pct
   xlab("") + ylab("") +
   ggtitle("Multi-Mapped")
 
-gg_bwameth_dup = ggplot(subset(df_map, pctDuplicated < 20), aes(x=platform, y=pctDuplicated)) +
+gg_bwameth_dup = ggplot(df_map, aes(x=platform, y=pctDuplicated)) +
   geom_boxplot(aes(color=platform, fill=platform), alpha=0.5, show.legend=F) +
   geom_beeswarm(aes(fill=platform, shape=genome), alpha=0.5, show.legend=F, size=3) +
   geom_hline(yintercept = 0) +
@@ -175,7 +142,7 @@ gg_bwameth_dup = ggplot(subset(df_map, pctDuplicated < 20), aes(x=platform, y=pc
   xlab("") + ylab("") +
   ggtitle("Duplicated")
 
-gg_bwameth_unmapped = ggplot(subset(df_map, pctUnmap < 5), aes(x=platform, y=pctUnmap)) +
+gg_bwameth_unmapped = ggplot(df_map, aes(x=platform, y=pctUnmap)) +
   geom_boxplot(aes(color=platform, fill=platform), alpha=0.5, show.legend=F) +
   geom_beeswarm(aes(fill=platform, shape=genome), alpha=0.5, show.legend=F, size=3, cex = 0.3) +
   geom_vline(xintercept = c(8.5, 10.5), linetype="dotted") +
@@ -189,16 +156,38 @@ gg_bwameth_unmapped = ggplot(subset(df_map, pctUnmap < 5), aes(x=platform, y=pct
 
 human_row = plot_grid(gg_bwameth_map, gg_bwameth_multi, gg_bwameth_dup, gg_bwameth_unmapped, nrow=1, rel_widths = c(3.2, 3, 2, 3))
 human_row
-```
-```{r, fig.width=10, fig.height=8}
+
+## Export
 pdf(file="figures/Fig1c_MappingHuman.pdf", width=12, height=3)
 human_row
 dev.off()
-```
+
 
 # Mapping Rates for Bacterial Genomes
 
-```{r, fig.width=12, fig.height=2.5}
+## bacterial data
+df_map_bact = read.csv("tables/mapping_stats_bacteria.csv")
+df_map_bact = separate(df_map_bact, col="sample", sep="_", into=c("platform", "lab", "genome", "rep"), remove=F)
+## label paired and single end
+df_map_bact$readType = ifelse(df_map_bact$platform == "MiSeq", "paired", "single")
+df_map_bact$readType2 = "bact"
+## get total bases / mean depth
+df_map_bact$totalBases = as.numeric(df_map_bact$readsTotal) * as.numeric(df_map_bact$avgLength)
+df_map_bact$meanDepth  = ifelse(df_map_bact$genome == "Eco", df_map_bact$totalBases/4641652, 
+                                ifelse(df_map_bact$genome == "Ste", df_map_bact$totalBases/2499279, 
+                                       ifelse(df_map_bact$genome == "Pflu", df_map_bact$totalBases/6511547, df_map_bact$totalBases/39879740)))
+## get percentages
+df_map_bact$pctProperlyPaired = df_map_bact$readsProperlyPaired / df_map_bact$readsTotal * 100
+df_map_bact$pctDuplicated = df_map_bact$readsDuplicated / df_map_bact$readsTotal * 100
+df_map_bact$pctMultimap = df_map_bact$readsMultimap / df_map_bact$readsTotal * 100
+df_map_bact$pctUnmap = df_map_bact$readsUnmap / df_map_bact$readsTotal * 100
+## re-order factors
+df_map_bact$platform = factor(df_map_bact$platform, levels=c("MiSeq", "Genapsys", "S5", "PGM"))
+df_map_bact$genome = factor(df_map_bact$genome, levels=c("Eco", "Ste", "Pflu", "Pool"))
+df_map_bact$readType = factor(df_map_bact$readType, levels=c("paired", "single"))
+
+
+## arrange plots
 gg_bacteria_map = ggplot(subset(df_map_bact), aes(x=genome, y=pctProperlyPaired)) +
   geom_boxplot(aes(color=platform, fill=platform), alpha=0.5, show.legend=F) +
   geom_beeswarm(aes(fill=platform), alpha=0.5, dodge.width=0.75, shape=21, show.legend=F, size=2) +
@@ -240,26 +229,23 @@ gg_bacteria_unmapped = ggplot(subset(df_map_bact), aes(x=genome, y=pctUnmap)) +
   ggtitle("Unmapped")
 bact_row = plot_grid(gg_bacteria_map, gg_bacteria_multi, gg_bacteria_dup, gg_bacteria_unmapped,  nrow=1, align='h', axis='b', rel_widths = c(3.2, 3, 2, 3))
 bact_row
-```
-```{r, fig.width=10, fig.height=8}
+
+## Export
 pdf(file="figures/Fig1d_MappingBact.pdf", width=12, height=2.5)
 bact_row
 dev.off()
-```
+
 
 # Insert Size Distrbution
 
-```{r}
+## set up matrix
 df_insert = read.csv("tables/insert_size_all.csv")
 df_insert = df_insert %>% group_by(sampleOverall, size) %>% mutate(freq = mean(freq))
 df_insert = df_insert[, c("sampleOverall", "size", "freq")]
 colnames(df_insert) = c("sample", "size", "freq")
 df_insert = separate(df_insert, col="sample", sep="_", into=c("platform", "lab", "genome", "rep"), remove=F)
 df_insert$platform = factor(df_insert$platform, levels=c("HiSeq2500", "HiSeq4000", "HiSeqX10", "BGISEQ500", "MGISEQ2000", "NovaSeq2x150", "NovaSeq2x250"))
-head(df_insert)
-```
 
-```{r}
 label_insert = c("HiSeq2500" = "HiSeq\n2500", 
                  "HiSeq4000" = "HiSeq\n4000", 
                  "HiSeqX10" = "HiSeq\nX10", 
@@ -267,10 +253,8 @@ label_insert = c("HiSeq2500" = "HiSeq\n2500",
                  "MGISEQ2000" = "MGISEQ\n2000", 
                  "NovaSeq2x150" = "NovaSeq\n2x150", 
                  "NovaSeq2x250" = "NovaSeq\n2x250")
-```
 
-
-```{r, fig.height=10, fig.width=6}
+## Plot
 gg_insert = ggplot(df_insert, aes(x=size, y=sample, height=freq, fill=platform)) +
   geom_ridgeline(scale=1, show.legend = F, alpha=0.7) +
   scale_fill_discrete(guide = guide_legend(reverse=TRUE)) +
@@ -284,54 +268,47 @@ gg_insert = ggplot(df_insert, aes(x=size, y=sample, height=freq, fill=platform))
   facet_grid(platform+lab~., scales="free_y", switch="y",
              labeller = labeller(platform = label_insert)) + 
   xlab("Insert size")
-gg_insert
-```
 
-```{r}
+## Export
 pdf(file="figures/InsertSize.pdf", width=6, height=10)
 gg_insert
 dev.off()
-```
 
 
 # GC Content Heatmap (Human)
 
-```{r}
-#df_gc = read.csv("tables/GC_normalizedCoverage_all.csv")
+## Set up matrix
 df_gc = read.csv("tables/GC_rawCoverage_all.csv")
 df_gc = df_gc %>% group_by(sampleOverall, GC) %>% mutate(normcov = mean(normcov))
 df_gc = df_gc[, c("sampleOverall", "GC", "normcov")]
 colnames(df_gc) = c("sample", "GC", "normcov")
 
-# go wide
+## go wide
 df_gc_tmp = dcast(data = df_gc, formula = ...~sample, value.var="normcov", fun.aggregate=sum)
 
-# normalize values
+## normalize values
 df_gc2 = apply(df_gc_tmp[, colnames(df_gc_tmp)[2:ncol(df_gc_tmp)]],2,normalized)
 
-# melt again
+## melt again
 df_gc2 = melt(df_gc2)
 colnames(df_gc2) = c("GC", "Sample", "Reads_Used")
 df_gc2$GC = df_gc2$GC - 1
 df_gc2 = separate(df_gc2, col=Sample, into=c("Platform", "Lab", "Genome", "Rep"), remove=F)
 
-# go wide again
+## go wide again
 df_gc = dcast(data = df_gc2, formula = ...~GC, value.var="Reads_Used",  fun.aggregate=mean)
 
-# create numeric matrix
+## create numeric matrix
 df_gc_m = as.matrix(df_gc[,6:ncol(df_gc)])
 df_gc_m[is.na(df_gc_m)] = 0
 rownames(df_gc_m) = df_gc[,1]
 df_gc_m[1:5, 1:10]
 
-# add WGS versus exome
+## add WGS versus exome
 df_gc$capture = ifelse(df_gc$Platform %in% c("Proton", "S5"), "exome", "WGS")
 df_gc$capture = factor(df_gc$capture, levels=c("WGS", "exome"))
-```
 
-
-```{r}
-# heatmap annotations
+## heatmap annotations
 ha_names = HeatmapAnnotation(Platform = df_gc$Platform,
                              col = list(Platform = c("HiSeq2500" = "#9ecae1", 
                                                      "HiSeq4000" = "#6baed6", 
@@ -347,14 +324,11 @@ ha_names = HeatmapAnnotation(Platform = df_gc$Platform,
                                                      #"PacBioCLR" = "#ef3b2c", 
                                                      #"PacBioCCS" = "#a50f15",
                                                      "Proton" = "#fc8d59", "S5" = "#fee08b")), which="row")
-
 ha_genomes = HeatmapAnnotation(Genome = df_gc$Genome,
                              col = list(Genome = c("Mother"="#FBB4AE", 
                                                    "Father"="#B3CDE3", 
                                                    "Son"="#CCEBC5")), which="row")
-```
-
-```{r, fig.width=7.5, fig.height=9}
+## plot
 hm = Heatmap(subset(df_gc_m),
              col = colorRamp2(c(0,0.5,1), c( "#2c7bb6","#ffffbf","#d7191c")),
              cluster_columns = FALSE,
@@ -375,51 +349,35 @@ hm = Heatmap(subset(df_gc_m),
                    title = "Normalized\nCoverage"
                  ))
 
-draw(hm + ha_names + ha_genomes, merge_legend = TRUE, heatmap_legend_side="right")
-```
 
-```{r}
+## Export
 pdf(file="figures/Supp_HeatmapHuman.pdf", width=7.5, height=10)
 draw(hm + ha_names + ha_genomes, merge_legend = TRUE, heatmap_legend_side="right")
 dev.off()
-```
-
-
 
 
 # GC Content Heatmap (Bacteria)
 
-```{r}
+## set up matrix
 df_gc_bact = read.csv("tables/bacteria_GC_rawCoverage_all.csv")
 #df_gc_bact = read.csv("tables/GC_rawCoverage_all.csv")
 df_gc_bact = df_gc_bact %>% group_by(sampleOverall, GC) %>% mutate(normcov = mean(normcov))
 df_gc_bact = df_gc_bact[, c("sampleOverall", "GC", "normcov")]
 colnames(df_gc_bact) = c("sample", "GC", "normcov")
-df_gc_bact
-```
 
-```{r}
 df_bac_gc_tmp = dcast(data = df_gc_bact, formula = ...~Sample, value.var="normcov", fun.aggregate=sum)
-df_bac_gc_tmp[1:10, 1:5]
-```
-```{r}
-df_bac_gc2 = apply(df_bac_gc_tmp[, colnames(df_bac_gc_tmp)[2:ncol(df_bac_gc_tmp)]],2,normalized)
-df_bac_gc2[1:5, 1:5]
-```
 
-```{r}
+df_bac_gc2 = apply(df_bac_gc_tmp[, colnames(df_bac_gc_tmp)[2:ncol(df_bac_gc_tmp)]],2,normalized)
+
 df_bac_gc2 = melt(df_bac_gc2)
 colnames(df_bac_gc2) = c("GC", "Sample", "Reads_Used")
 df_bac_gc2$GC = df_bac_gc2$GC - 1
 df_bac_gc2 = separate(df_bac_gc2, col=Sample, into=c("Platform", "Lab", "Genome", "Rep"), remove=F)
-head(df_bac_gc2)
-```
 
-```{r}
-# convert tall to wide
+## convert tall to wide
 df_gc_bact = dcast(data = df_bac_gc2, formula = ...~GC, value.var="Reads_Used",  fun.aggregate=mean)
 
-# create metadata
+## create metadata
 #df_gc_bact = separate(df_gc_bact, col="Sample", sep="_", into=c("platform", "lab", "genome", "rep"), remove=F)
 #df_gc_bact$platform = factor(df_gc_bact$platform, levels=c("HiSeq2500", "HiSeq4000", "HiSeqX10", "BGISEQ500", "MGISEQ2000", "NovaSeq2x150", "NovaSeq2x250", "Genapsys", "Proton", "S5"))
 
@@ -427,11 +385,8 @@ df_gc_bact = dcast(data = df_bac_gc2, formula = ...~GC, value.var="Reads_Used", 
 df_gc_bact_m = as.matrix(df_gc_bact[,6:ncol(df_gc_bact)])
 df_gc_bact_m[is.na(df_gc_bact_m)] = 0
 rownames(df_gc_bact_m) = df_gc_bact[,1]
-df_gc_bact_m[1:5, 1:10]
-```
 
-```{r}
-# heatmap annotations
+## heatmap annotations
 ha_names = HeatmapAnnotation(Platform = df_gc_bact$Platform,
                              col = list(Platform = c("MiSeq"="#7fc97f", "S5"="#fdc086", "PGM"="#386cb0", "Genapsys"="#f0027f")), which="row")
 
@@ -439,9 +394,9 @@ ha_genomes = HeatmapAnnotation(Genome = df_gc_bact$Genome,
                              col = list(Genome = c("Mother"="#FBB4AE", 
                                                    "Father"="#B3CDE3", 
                                                    "Son"="#CCEBC5")), which="row")
-```
 
-```{r, fig.width=7.5, fig.height=9}
+
+## Plot
 hm = Heatmap(subset(df_gc_bact_m),
              col = colorRamp2(c(0,0.5,1), c( "#2c7bb6","#ffffbf","#d7191c")),
              cluster_columns = FALSE,
@@ -462,16 +417,12 @@ hm = Heatmap(subset(df_gc_bact_m),
                    title = "Normalized\nCoverage"
                  ))
 
-draw(hm + ha_names, merge_legend = TRUE, heatmap_legend_side="right")
-```
-
-```{r}
+## Export
 pdf(file="figures/Supp__HeatmapBact.pdf", width=7.5, height=7)
 draw(hm + ha_names, merge_legend = TRUE, heatmap_legend_side="right")
 dev.off()
-```
 
-```{r}
+# Entropy plot
 bact_entropy = as.data.frame(matrix(nrow=19, ncol=4))
 colnames(bact_entropy) = c("Ste", "Pool", "Eco", "Pflu")
 
@@ -484,11 +435,8 @@ bact_entropy$Pflu = entropy(df_gc_bact[df_gc_bact$Genome=="Pflu", 6:82])[1:19]
 
 bact_entropym = melt(bact_entropy)
 colnames(bact_entropym) = c("Genome", "entropy")
-bact_entropym
-```
 
-```{r, fig.width=5}
+## Export
 pdf(file="figures/Supp__HeatmapBactEntropy.pdf", width=5, height=3)
 ggplot(bact_entropym, aes(x=Genome, y=entropy)) + geom_boxplot(aes(fill=Genome)) + geom_point(shape=21) + theme_jon
 dev.off()
-```
